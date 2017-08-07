db1400fe8fc4d467594c14e87b6be32cd1b9e78c
itskp <kapilgajarmal@gmail.com>
2017-08-07 20:58:56 +0530

    urls updated

--- src/todo/urls.js
+++ src/todo/urls.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 const todoView = require('./views.js')
 
-router.post('/', todoView.getTodos)
+router.post('/get', todoView.getTodos)
 router.post('/create', todoView.createTodo)
 
 module.exports = router
