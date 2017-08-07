afbf013453fc9dbf78268b868490be20ab13d84f
itskp <kapilgajarmal@gmail.com>
2017-08-08 01:06:46 +0530

    added all complete url

--- src/todo/urls.js
+++ src/todo/urls.js
@@ -3,5 +3,7 @@ const todoView = require('./views.js')
 
 router.post('/get', todoView.getTodos)
 router.post('/create', todoView.createTodo)
+router.post('/updateStatus', todoView.updateTodoStatus)
+router.post('/markAll', todoView.markAll)
 
 module.exports = router
--- src/todo/views.js
+++ src/todo/views.js
@@ -9,13 +9,33 @@ function* getTodos() {
 function* createTodo() {
     const body = this.request.body
 
-    yield db.todos.create(body)
-    this.body = 'created todo'
+    const result = yield db.todos.create(body)
+    this.body = result
+    this.status = 200
+}
+
+function* updateTodoStatus() {
+    const body = this.request.body
+    const result = yield db.todos.findByIdAndUpdate(body.todoid, {
+        'status': body.status == 0 ? 1:0,
+    }, {
+        new: true
+    })
+    this.body = result
+    this.status = 200
+}
+function* markAll(){
+    const body = this.request.body
+    yield db.todos.update({},{'status':body.status},{multi:true})
+    const result = yield db.todos.find()
+    this.body = result
     this.status = 200
 }
 
 module.exports = {
     getTodos,
-    createTodo
+    createTodo,
+    updateTodoStatus,
+    markAll
 }
 
